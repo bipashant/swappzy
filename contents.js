8c8b3d1a0c9de615016df033168f2e1fd3211834
Bibek Sharma Chapagain <bs_chapagain@hotmail.com>
2020-04-23 12:12:29 +1000

    Create contents.js

---
+++ contents.js
@@ -0,0 +1,109 @@
+/**
+ * Created by bishant on 4/23/20.
+ */
+var questionAnswerForSeller = [
+
+    {
+        "qns": "How do I sign up as a seller?",
+        "ans": "Sign up on Swappzy by clicking on the “Post a game for Sale” button on the top right-hand side of the home page.  Create an account by using your Facebook login information or by entering your email address, name and password.  Once you create an account, you can begin selling your items.  Please note that you will need to link your PayPal account, enter your bank account details, or both, to receive payments as your items sell.  This info needs to be added to the payment section of your settings in your Swappzy profile.  To access your settings menu simply click on your profile image."
+    },
+    {
+        "qns": "How do I list an item on Swappzy?",
+        "ans": "First, you will need to sign up.  Once you are logged in, simply click on the “Post a game for Sale” button.  This can be found on the top right-hand corner of the home screen on your desktop or by clicking the drop-down menu on the left-hand side of your mobile home screen. You will then be guided through a series of simple steps to complete your listing."
+    },
+    {
+        "qns": "My item has been purchased.  Why is it still on the site?",
+        "ans": "Until you manually close your item it will be visible on the site.  To close your listing, click on the listing item and select the option to “close listing”.  This can be found on the right-hand side of the screen on your desktop or at the bottom of your listing details in mobile view."
+    },
+    {
+        "qns": "My photos are not uploading in the order that I want them to appear, can I change this?",
+        "ans": "Yes.  Once you have uploaded your images, you can drag and drop them into any position.  If there is already an image in the position that you want to put another image into, simply drag and drop your selected image into that spot and the original image will move itself into another position."
+    },
+    {
+        "qns": "What are the fees to use Swappzy?",
+        "ans": "It is completely free to sign up and free to list items for sale.  Swappzy only charges a commission on items that are sold.  This fee depends on which payment processing service(s) you choose to use."
+    },
+    {
+        "qns": "Who does Swappzy use to process its payments?",
+        "ans": "Swappzy uses Stripe and PayPal to process payments.  As a seller, you can choose which payment method you want to accept, and have the option to accept both.  You can find out more about them at www.stripe.com and www.paypal.com"
+    },
+    {
+        "qns": "Are my bank account details safe on Swappzy?",
+        "ans": "Yes. If you choose to accept payments via Stripe, you will need to enter your bank account to receive your money.  Stripe has been audited by a PCI-certified auditor and is certified to PCI Service Provider Level 1.  This is the most stringent level of certification available in the payments industry.  Visit www.stripe.com to find out more about security at Stripe."
+    },
+    {
+        "qns": "Are credit card payments safe on Swappzy?",
+        "ans": "Yes. Swappzy uses Stripe and PayPal to process all payments.  Stripe has been audited by a PCI-certified auditor and is certified to PCI Service Provider Level 1.  This is the most stringent level of certification available in the payments industry.  Visit www.stripe.com to find out more about security at Stripe.  With PayPal, buyers and sellers only need to use their PayPal credentials to send and receive money, and do not need to provide financial account information on the site."
+    },
+    {
+        "qns": "When will I get paid?",
+        "ans": "The timing of when you get paid varies depending on whether the transaction is paid for via Stripe or PayPal. With Stripe, once you accept a transaction, it can take up to 10 days for Stripe to deposit money into your bank account. Rest assured that you will get your money. Prior to purchasing your item, a buyer must enter credit card details and the money is held in escrow with Stripe until the funds are released to you. With PayPal, the money moves to your account automatically when you accept the transaction. There is no holding period."
+    },
+    {
+        "qns": "I have accepted the buyer’s payment but it’s not in my bank account. What’s going on?",
+        "ans": "If you use Stripe, it can take up to 10 days to receive your payment into your bank account, once you accept a transaction. If you haven’t received your money within 10 days, please contact us at info@swappzy.com and we will do what we can to help.  If you use PayPal, you should receive a payment into your PayPal account once you accept a transaction."
+    },
+    {
+        "qns": "What if I want to change my listing once it has been posted?",
+        "ans": "All listings can be edited at any time, even once they are live on the site.  This includes changing the price, description, images or anything else.  You can find the edit button on the right-hand side of the listing in your desk top view or at the very bottom of the listing in mobile view."
+    },
+    {
+        "qns": "How can I contact a buyer?",
+        "ans": "To contact your buyer, you use your Swappzy inbox.  As soon as a buyer purchases your item, you are put into contact via our internal messaging service.  Please refer to our Terms of Use for guidelines around communicating with other people on Swappzy."
+    },
+    {
+        "qns": "What should I do if there isn’t a category for my item?",
+        "ans": "Please contact us on helllo@swappzy.com with details of the category that you would like.  We will evaluate your request and make every attempt to accommodate it."
+    },
+    {
+        "qns": "How do I receive my payments?",
+        "ans": "If you choose to use Stripe, you will need to add your bank account details in the “payment” section of your “settings” panel.  Once you have done this, your payment will go directly into your bank account.  You can find out more about them at www.stripe.com.  If you choose to use PayPal, you just need to link your PayPal account using your PayPal credentials and payments will settle directly into your PayPal account."
+    },
+    {
+        "qns": "I've read through the FAQs and still have questions.  Who do I contact?",
+        "ans": "If you have general questions about Swappzy, we are always happy to help.  Please contact us at helllo@swappzy.com."
+    }
+];
+
+var questionAnswerForShopper = [
+    {
+        "qns": "How do I sign up?",
+        "ans": "To sign up on Swappzy,  click on the sign up button on the top right hand side of the home page.  You will be directed to either sign up with your Facebook login information or create an account with us by providing your email address, name and password. Once you have completed the steps as directed you will be signed up and ready to go!"
+    },
+    {
+        "qns": "How do I reset my password?",
+        "ans": "Please visit the “Log in” page and select “forgot username or password” under the log-in fields.  You will be guided through the steps to create a new password from there."
+    },
+    {
+        "qns": "Why hasn’t my payment been accepted / processed?",
+        "ans": "In order for your payment to be completed, the seller must accept it in their Swappzy dashboard.  This may take a little time, but we encourage sellers to respond as quickly as possible.  You will be notified as soon as the seller has approved your payment.  If you are concerned about the time it is taking for a payment to be accepted, you can contact the seller directly via the Swappzy inbox."
+    },
+    {
+        "qns": "Is my credit card information safe on Swappzy?",
+        "ans": "Yes. Swappzy uses Stripe and PayPal to process all payments.  Stripe has been audited by a PCI-certified auditor and is certified to PCI Service Provider Level 1.  This is the most stringent level of certification available in the payments industry.  Visit www.stripe.com to find out more about security at Stripe.  If you choose to pay using PayPal, you do not have to share your financial information with us; you can just log into your PayPal account.  PayPal uses sophisticated encryption to ensure the security of your transaction.  PayPal also offers buyer protection services.  You can read more about PayPal security by visiting https://www.paypal.com/us/webapps/mpp/paypal-safety-and-security"
+    },
+    {
+        "qns": "How can I contact a seller?",
+        "ans": "You can contact any seller by clicking the “contact” button on their listing page.  This can be found on the right-hand side of the listing page in desktop view or at the bottom of the listing info in mobile view."
+    },
+    {
+        "qns": "My item didn’t arrive.  What should I do?",
+        "ans": "If you haven't received an item, please do not mark your transaction as completed.  You need to make contact with the seller directly to get an update on delivery. If you find that the seller’s response is unsatisfactory, you can get in touch with Swappzy directly and we will contact the seller and review the issue on your behalf.  Please note that Swappzy will not issue any refunds on the seller’s behalf. Refunds must be paid from the seller directly. Please refer to our Terms of Use for more information on disputes and refunds."
+    },
+    {
+        "qns": "My item is not as described in the listing.  What should I do?",
+        "ans": "If there are any significant discrepancies with the item when you receive it, do not mark your transaction as completed.  You should contact the seller directly. The seller will address your concerns directly. If both you and the seller have agreed on returning the item, you must return the item in the exact same condition in which it was received."
+    },
+    {
+        "qns": "Can I get a refund?",
+        "ans": "All sellers are able to refund the cost of an item if this is mutually agreed between both parties.  Swappzy will not issue any refunds on the seller’s behalf.  Refunds must be paid from the seller directly. Please refer to our Terms of Use for more information on disputes and refunds."
+    },
+    {
+        "qns": "Can I sell on Swappzy?",
+        "ans": "Yes!  Anyone can sell on Swappzy.  Once you’ve created an account, you have the option to post a listing.  You will need to provide bank account details so that you can receive funds for any items that you sell."
+    },
+    {
+        "qns": "I’ve read through the FAQs and I still have questions.  Who do I contact?",
+        "ans": "If your question relates to a listing or item that you’ve purchased, please reach out to the seller.  If you have general questions about Swappzy, we are always happy to help.  Please contact us at helllo@swappzy.com"
+    }
+];
